2085ff9249eeb514988fa0d0ea5cd48ebd32ad44
Connie <conniedpaula@gmail.com>
2023-01-02 15:15:27 -0300

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button @click="onClick()" :style="{ background: color }" class="btn">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: {
+            type: String,
+            default: 'Add task'
+        },
+        color: {
+            type: String,
+            default: 'steelblue'
+        }
+    },
+    methods: {
+        onClick() {
+            this.$emit('btn-click')
+        }
+    }
+}
+</script>
